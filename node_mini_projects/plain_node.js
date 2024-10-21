126273c3aa84c46fef7739766167233806de62bf
Forester04 <erickforester04@gmail.com>
2024-10-21 22:56:39 +0300

    Add chat enndpoint and the sse function

--- node_mini_projects/plain_node.js
+++ node_mini_projects/plain_node.js
@@ -3,6 +3,9 @@
 const http = require('http');
 const fs = require('fs');
 const querystring = require('querystring')
+const EventEmitter = require('events');
+
+const chatEmitter = new EventEmitter()
 
 const port = process.env.PORT | 3000
 
@@ -10,6 +13,10 @@ const server = http.createServer( function(req, res) {
     if(req.url == '/') return respondText(req, res);
     if(req.url == '/json') return respondJson(req, res);
     if(req.url.match(/^\/echo/)) return respondEcho(req, res);
+    if(req.url.match(/^\/static\/.*/)) return respondStatic(req, res);
+    if(req.url.match(/^\/chat/)) return respondChat(req, res)
+    if(req.url.match(/^\/sse/)) return respondSSE(req, res)
+
 
     return respondNotFound(req, res);
 })
@@ -23,7 +30,7 @@ function respondText(req, res) {
 
 function respondJson(req, res) {
     res.setHeader('Content-type', 'application/json');
-    res.end(JSON.stringify({name: 'Forester', number: [1, 2, 3, 4]}));
+    res.end(JSON.stringify({name: 'Real-Time Chat App', version: "1.01"}));
 }
 
 function respondEcho(req, res) {
@@ -44,6 +51,40 @@ function respondEcho(req, res) {
         .reverse()
         .join('')
     }))
+}
+
+function respondStatic(req, res) {
+    const filepath = req.url.split('/static')[1];
+
+    const filename = `${__dirname}/static${filepath}`;
+
+    fs.createReadStream(filename)
+    .on('error', () => respondNotFound(req, res))
+    .pipe(res)
+}
+
+function respondChat(req, res) {
+    const { message } = querystring.parse()
+
+    chatEmitter.emit('message', message)
+    res.end()
+    new window.EventSource('/sse').onmessage = function (event) {
+    window.messages.innerHTML += `<p>${event.data}<p>`
+    }
+}
+
+function respondSSE(req, res) {
+    res.writeHead(200, {
+        'Content-Type' : 'text/event-stream',
+        'Connection' : 'keep-alive'
+    })
+
+    const onMessage = msg => res.write(`data: ${msg}\n\n`)
+    chatEmitter.on('message', onMessage)
+
+    res.on('close', function () {
+        chatEmitter.off('message', onMessage)
+    })
 }
 
 function respondNotFound(req, res) {
