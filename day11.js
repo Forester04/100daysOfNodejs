3c69fe36236a7e6b01a162a12f174ec9d3b84eaa
Forester04 <erickforester04@gmail.com>
2024-11-03 20:58:48 +0300

    Day 11 of 100daysOfnodejs

---
+++ day11.js
@@ -0,0 +1,7 @@
+// Day 11 - Static Files with Express
+// Serve static files using Express middleware
+/*
+ *First install the serve-static to server files
+ * We will use the serve-static middleware to serve files
+*/
+
