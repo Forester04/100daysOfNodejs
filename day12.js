a11d7440caa9c87c3e94a837191f74644d3e0aa3
Forester04 <erickforester04@gmail.com>
2024-11-04 13:49:26 +0300

    Day 12: Handling query parameters

--- day12.js
+++ day12.js
@@ -1,3 +1,21 @@
 // 100 Days of Nodejs challenges
 // Day 12 - Query Parameters
 // Handle query parameters in an Express app
+/*
+ * Before handling the query parameters in an Express app. Let's first break down the concept to undestand more about query parameter.
+ * Query parameters are key value paris the are appended after a ? in a URL. Often GET method use the query parameters to filter, search annd add additional information to the server endpoint. 
+ * Handling the query parameters in Express is straight forward. The req object which represents the request sent to the server. The req object contains two objects: req.param and req.query; On req.query - on this property you can go ahead and access the key and value.
+ * The router handler in express extracts the parameter from the url
+*/
+
+const express = require('express');
+const port = process.env.PORT || 1337;
+
+const app = express();
+
+app.get('/search', (req, res) => {
+    const queryTerm = req.query.query;
+    const category = req.query.category;
+
+    res.send(`Search query ${queryTerm}, Category ${category}`);
+})
