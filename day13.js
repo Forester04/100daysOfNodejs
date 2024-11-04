bdd956e3a354f67e386f3102b0b6794d32279926
Forester04 <erickforester04@gmail.com>
2024-11-04 17:58:59 +0300

    Day 13: Desiging a simple RESTful API with Express

--- day13.js
+++ day13.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { body, param, validationResult } = require('express-validator');
+const { validate } = require('uuid');
 
 const port = process.env.PORT || 1337;
 const app = express();
@@ -7,17 +9,42 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+// validation and error handling middleware
+const validateResult = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).send({ errors: errors.array() });
+    }
+    next();
+}
+
 app.get('/', (req, res) => {
     res.send('Book App: Simple RESTfull API');
 });
-app.post('/book', (req, res) => {
-    res.send('Created book successfully');
+app.post('/book', 
+    body('title').notEmpty().withMessage('Title is required'),
+    body('author').notEmpty().withMessage('Author name is required'),
+    validateResult,
+    (req, res) => {
+        res.status(201).send({ message: 'Created book successfully', data: req.body })
 });
-app.put('/book/:id', (req, res) => {
-    res.send('Updated book successfully using book id');
+app.put('/book/:id', 
+    book('id').isNumeric().withMessage('Book id required'),
+    body('title').notEmpty().withMessage('Title is required'),
+    body('author').notEmpty().withMessage('Author name is required'),
+    validateResult,
+    (req, res) => {
+    res.status(200).send({ message: 'Updated book successfully using book id' });
 });
 app.delete('/book/:id', (req, res) => {
     res.send('Book deleted successfully');
 });
+
+// Global error handling
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send({ message: 'Internal Error occured'});
+})
 
 app.listen(port, () => console.log(`Server listenig on ${port}`));
--- test.js
+++ test.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { body, param, validationResult} = require('express-validator');
 
 const port = process.env.PORT || 1337;
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+// Validation and Error Handling Middleware
+const validateRequest = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+};
 
 app.get('/', (req, res) => {
     res.send('My first API');
@@ -27,4 +37,12 @@ app.post('/submit', (req, res) => {
     console.log(firstname)
     console.log(lastname);
 })
+
+
+// Global Error handling
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send({ message: 'An unexpected error occurred' });
+});
+
 app.listen(port, () => console.log(`Server listening on ${port}`));
