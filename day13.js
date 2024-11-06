4dc7e5515bcc94bad70e5043565deb60375a2953
Forester04 <erickforester04@gmail.com>
2024-11-06 22:51:11 +0300

    Revamp of day 13

--- day13.js
+++ day13.js
@@ -1,7 +1,11 @@
+// 100 Days of Nodejs challenges
+// Day 13 - RESTful API Design
+// Design a simple RESTful API using Express
+
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const { body, param, validationResult } = require('express-validator');
-const { validate } = require('uuid');
 
 const port = process.env.PORT || 1337;
 const app = express();
@@ -10,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 // validation and error handling middleware
-const validateResult = (req, res, next) => {
+const validateRequest = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).send({ errors: errors.array() });
@@ -24,19 +28,22 @@ app.get('/', (req, res) => {
 app.post('/book', 
     body('title').notEmpty().withMessage('Title is required'),
     body('author').notEmpty().withMessage('Author name is required'),
-    validateResult,
+    validateRequest,
     (req, res) => {
         res.status(201).send({ message: 'Created book successfully', data: req.body })
 });
 app.put('/book/:id', 
     book('id').isNumeric().withMessage('Book id required'),
-    body('title').notEmpty().withMessage('Title is required'),
-    body('author').notEmpty().withMessage('Author name is required'),
-    validateResult,
+    body('title').optional().notEmpty().withMessage('Title is required'),
+    body('author').optional().notEmpty().withMessage('Author name is required'),
+    validateRequest,
     (req, res) => {
     res.status(200).send({ message: 'Updated book successfully using book id' });
 });
-app.delete('/book/:id', (req, res) => {
+app.delete('/book/:id', 
+    book('id').isNumeric().withMessage('Book id is required'),
+    validateRequest,
+    (req, res) => {
     res.send('Book deleted successfully');
 });
 
