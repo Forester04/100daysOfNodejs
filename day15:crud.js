2f3a5a39e954ba60aadba4e5ac3c5910fe44d64c
Forester04 <erickforester04@gmail.com>
2024-11-09 00:21:32 +0300

    Code update

--- day15:crud.js
+++ day15:crud.js
@@ -2,108 +2,99 @@
 // Day 15 - CRUD Operations with MongoDB
 // Perform CRUD operations using MongoDB and the mongodb Nodejs driver
 
-const express = require('express');
 const bodyParser = require('body-parser');
+const express = require('express');
+const { body,  param, validationResult } = require('express-validator')
+const { MongoClient } = require('mongodb');
 const jwt = require('jsonwebtoken');
-const { MongoClient, ObjectId } = require('mongodb');
-const { body, param, validationResult } = require('express-validator');
+const ObjectId = require('mongodb').ObjectId;
+
 
 const port = process.env.PORT || 1337;
 const app = express();
 
-// Middlewares
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true}))
 
-//Global validation
-app.use((err, req, res, next) => {
+
+// Dummy User
+const user = {
+    userId: 1,
+    username: 'John Doe',
+    password: 'abcdefghi'
+}
+// Secret Key
+const secretKey = 'your_secret_key';
+
+// DB connection
+const uri = "mongodb://localhost:1337";
+const client = new MongoClient(uri);
+
+// Global validation
+app.use((err, req, res) => {
     if (err) {
         console.error(err);
         res.status(500).send('Internal Error');
     }
 })
-const validateRequest = (req, res, next) => {
-    const errors = validationResult;
+
+const validateRequest = (req, res) => {
+    const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).send({ errors: errors.array()});
+        res.status(400).json({ errors: errors.array()});
     }
 }
-
 const authenticateToken = (req, res, next) => {
     const token = req.header('authorization')?.split(' ')[1];
     if (!token) {
-        return res.status(403).send({ message: 'Authentication Denied'});
+        res.status(403).send(' Authentcation Denied');
     }
+
     jwt.verify(token, secretKey, (err, decode) => {
         if (err) {
-            return res.status(403).send({ message: 'Authentication Denied'}); 
+            return res.status(403).send('Authenication Denied');
         }
         req.user = decode;
         next();
     })
 }
 
-// DB Connections
-const uri = 'mongodb://localhost:1337';
-const client = new MongoClient(uri);
-const dbName = 'Library';
+app.get('/login',
+    (req, res) => {
+        const { username, password } = req.body;
 
-// Dummy user
-const user = {
-    userId: 1,
-    username: 'John Doe',
-    password: 'abcdefghi'
-}
-// SecretKey
-const secretKey = 'your_secret_key';
+        if (username === username.username && password === user.password) {
+            const token = jwt.sign({ userId: user.userId}, secretKey, { expiresIn: '1hr'});
 
-app.get('/login', (req, res) => {
-    const { username, password } = req.body;
-
-    if ( username === user.username && password === user.password ) {
-        const token = jwt.sign({ userId: user.id}, secretKey, { expiresIn: '1hr'});
-        return res.status(200).send({ message: 'Logging in successful'});
-    } else {
-        return res.status(400).send({ message: 'Invalid username or password'});
+            res.status(200).json({ message: 'Logged in successfully', token});
+        } else {
+            res.status(400).send('Invalid username or password');
+        }
     }
-})
+)
 
-
-// CRUD Operation with MongoDB
-async function performCrudOperations() {
+// Performing Crud operation on Protected routes
+async function run() {
     try {
         await client.connect();
         const db = client.db(dbName);
+        // CREATE
+        app.post('/book', authenticateToken, validateRequest, [body('title').notEmpty().withMessage('Title required'), body('author').notEmpty().withMessage('Author required')], async (req, res) => {
+            const { title, author } = req.body
+            
+            const results = await db.collection('books').insertOne({ title, author});
+            res.status(201).json({ message: 'Book added successfully', bookId: results.result.insertedId});
+        });
 
-        // CREATE - Protected route to add book
-        app.post('book',
-            authenticateToken,
-            [body('title').notEmpty().withMessage('Title required'),
-            body('author').notEmpty().withMessage("Author's name required")],
-            validateRequest,
-            async (req, res) => {
-                const { title, author } = req.body;
-                const result = await db.collection('books').insertOne({ title, author});
-                res.status(201).json({ message: 'Book added successfully'});
-            }
-        );
+        // READ
+        app.get('/books', authenticateToken, async (req, res) => {
+            const books = await db.collection('books').find().toArray();
+            res.status(200).json(books);
+        });
 
-        // READ - Protected route to read all books
-        app.get('/books', authenticateToken,
-            async (req, res) => {
-                const books = await db.collection('books').find().toArray();
-                res.status(200).json(books);
-            }
-        );
-
-        // UPDATE - Protected route to update a book by ID
-        app.put('/book/:id',
-            authenticateToken,
-            validateRequest,
-            [ body('title').optional().notEmpty().withMessage('Title required'),
-            body('author').optional().notEmpty().withMessage("Author's name required"), param('id').isMongoId().withMessage('Invalid book ID')
-            ],
-            async (req, res) => {
+        // UPDATE
+        app.put('/book/:id', authenticateToken, validateRequest,
+            body('title').optional().notEmpty().withMessage('Title required'), body('author').optional().notEmpty().withMessage('Author required'), param('id').isMongoId().withMessage('Invalid book ID'), async (req, res) => {
                 const { title, author } = req.body;
                 const { id } = req.params;
 
@@ -111,16 +102,12 @@ async function performCrudOperations() {
                 if (title) update.title = title;
                 if (author) update.author = author;
 
-                const result = await db.collection('books').updateOne({ _id: new inputId(id) }, { $set: update});
+                const results = await db.collection('books').updateOne({_id: new ObjectId() }, {$set: update});
+                if (results.matchedCount === 0) return res.status(404).json({ message: 'Book not found'});
             }
         )
     }
 }
-
-
-
-
-
 
 
 app.listen(port, () => console.log(`Server listening on ${port}`));
