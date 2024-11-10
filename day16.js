25d8480836e54cf1c0bb4b4012c14beabe693dca
Forester04 <erickforester04@gmail.com>
2024-11-10 23:55:17 +0300

    Basic server setup

--- day16.js
+++ day16.js
@@ -1,3 +1,18 @@
 // 100 Days of Nodejs challenges
 // Day 16 - Express Middleware for Error Handling
 // Implement middleware for handling errors in an Express app
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const port = process.env.PORT || 1337;
+
+
+const app = express();
+
+app.get('/', (req, res, next) => {
+    res.status(200).send('Error handling in Express');
+})
+
+app.listen(port, () => console.log(`Server listening on ${port}`));
