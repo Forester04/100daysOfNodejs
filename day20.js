e4545966ea789870527df7ce4b2d0813aed1940e
Forester04 <erickforester04@gmail.com>
2024-11-14 22:40:51 +0300

    Basic setup

--- day20.js
+++ day20.js
@@ -1,3 +1,23 @@
 // 100 Days of Nodejs challenges
 // Day 21 - Promises in Nodejs
 // Understand and use promises for asynchronous operations
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 1337;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.get('/', (req, res) => {
+    res.send('Promises for asynchronous operations')
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+})
