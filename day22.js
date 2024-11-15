86b4460084a3c0106bfcfbe1e80332252fccaa29
Forester04 <erickforester04@gmail.com>
2024-11-16 00:00:01 +0300

    Day 22 basic setup

--- day22.js
+++ day22.js
@@ -1,3 +1,24 @@
 // 100 Days of Nodejs challenges
 // Day 22 - Async/Await in Nodejs
 // Use async/await syntax for asynchronous programming
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 1337;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.get('/', (req, res) => {
+    res.send('Promises for asynchronous operations')
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+})
