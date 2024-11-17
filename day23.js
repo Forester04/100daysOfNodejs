f3ea1928fc2a337a02827e6de31a5d3be7d9efaa
Forester04 <erickforester04@gmail.com>
2024-11-17 23:29:44 +0300

    Basic setup

--- day23.js
+++ day23.js
@@ -1,3 +1,22 @@
 // 100 Days of Nodejs challenges
 // Day 23 - Basic WebSocket Server
 // Create a WebSocket server using the ws library
+
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const PORT = process.env.PORT || 1337;
+
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.get('/', (req, res) => {
+    res.send('Basic WebSockets');
+})
+
+
+app.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`));
