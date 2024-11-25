305f19c52b28a14afa99b1f571a82a2390eea290
Forester04 <erickforester04gmail.com>
2024-11-25 22:46:55 +0300

    code update

--- day23.js
+++ day23.js
@@ -3,20 +3,30 @@
 // Create a WebSocket server using the ws library
 
 const express = require('express');
-const bodyParser = require('body-parser');
-require('dotenv').config();
-const PORT = process.env.PORT || 1337;
+const { WebSocketServer } = require('ws');
 
+require('dotenv').config();
+const PORT = 1337;
+const WS_PORT = 8081
 
 const app = express();
 
-// Middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.get('/', (req, res) =>
+   res.sendFile('/websocket.html', { root: __dirname })
+ )
+ .listen(PORT, () => console.log(`Listening on ${PORT}`))
 
-app.get('/', (req, res) => {
-    res.send('Basic WebSockets');
-})
-
-
-app.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`));
+const socketServer = new WebSocketServer({ port: WS_PORT});
+socketServer.on('connection', ws => {
+    console.log('New client made a connection');
+    ws.send('Connection Established');
+    ws.on('message', data => {
+        socketServer.clients.forEach(client => {
+            console.log(`Distributing messages: ${data}`);
+            client.send(`${data}`);
+        })
+    })
+    ws.onerror = function () {
+        console.log('websocket error');
+    }
+});
