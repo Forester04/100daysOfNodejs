9008fbc227463a969fbca5407a46e6c962a81f27
Forester04 <erickforester04@gmail.com>
2024-12-01 22:43:06 +0300

    Basic setup

--- day25.js
+++ day25.js
@@ -1,3 +1,19 @@
 // 100 Days of Nodejs challenges
 // Day 25 - Testing with Mocha and Chai
 // Write basic tests using Mocha and Chai
+
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const PORT = process.env.PORT || 1337;
+
+const app = require('express');
+
+app.get('/', (req, res) => {
+    res.send('Day 25')
+})
+
+
+
+
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
